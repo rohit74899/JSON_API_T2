1374328c85fe412efc30965c2c4347ca9da3c982
rohit74899 <rohitlande02@gmail.com>
2023-12-22 07:49:46 +0530

    table and import function added

--- app.js
+++ app.js
@@ -1,3 +1,5 @@
+let jsonData = [];
+
 document.getElementById('fileInput').addEventListener('change', handleFileSelect);
 
 function handleFileSelect(event) {
@@ -6,27 +8,84 @@ function handleFileSelect(event) {
     if (file) {
         const reader = new FileReader();
         reader.onload = function (e) {
-            const jsonData = JSON.parse(e.target.result);
-            displayData(jsonData);
+            try {
+                // Parse the JSON data and ensure it's an array
+                jsonData = JSON.parse(e.target.result).products || [];
+                initializeFields();
+            } catch (error) {
+                console.error('Error parsing JSON:', error);
+                alert('Error parsing JSON. Please check the file format.');
+            }
         };
         reader.readAsText(file);
     }
 }
 
-function displayData(data) {
-    const tableContainer = document.getElementById('tableContainer');
-    const table = document.createElement('table');
+function initializeFields() {
+    const availableFields = document.getElementById('availableFields');
+    const thead = document.querySelector('#dataTable thead');
+    thead.innerHTML = '';
 
-    // Assuming data is an array of objects with attributes (subcategory, title, price, popularity)
-    data.forEach(product => {
-        const row = table.insertRow();
-        const titleCell = row.insertCell(0);
-        const priceCell = row.insertCell(1);
+    // Ensure jsonData is an array and not empty
+    if (Object.keys(jsonData).length > 0) {
+        // Assuming the first object in the data contains the column names
+        const columns = Object.keys(jsonData[Object.keys(jsonData)[0]]);
 
-        titleCell.textContent = product.title;
-        priceCell.textContent = product.price;
+        columns.forEach(column => {
+            const option = document.createElement('option');
+            option.value = column;
+            option.text = column;
+            availableFields.add(option);
+
+            const th = document.createElement('th');
+            th.textContent = column;
+            thead.appendChild(th);
+        });
+    } else {
+        console.error('JSON data is not in the expected format.');
+    }
+}
+
+function addSelected() {
+    moveSelectedOptions('availableFields', 'displayedFields');
+    displayData();
+}
+
+function removeSelected() {
+    moveSelectedOptions('displayedFields', 'availableFields');
+    displayData();
+}
+
+function moveSelectedOptions(sourceId, targetId) {
+    const source = document.getElementById(sourceId);
+    const target = document.getElementById(targetId);
+
+    Array.from(source.selectedOptions).forEach(option => {
+        target.add(option);
     });
 
-    tableContainer.innerHTML = ''; // Clear previous content
-    tableContainer.appendChild(table);
+    clearSelection(source);
+    clearSelection(target);
+}
+
+function clearSelection(select) {
+    select.selectedIndex = -1;
+}
+
+function displayData() {
+    const displayedFields = Array.from(document.getElementById('displayedFields').options).map(option => option.value);
+
+    const tbody = document.querySelector('#dataTable tbody');
+    tbody.innerHTML = '';
+
+    // Sort the data based on descending popularity
+    const sortedData = Object.entries(jsonData).sort(([, a], [, b]) => b.popularity - a.popularity);
+
+    sortedData.forEach(([key, item]) => {
+        const row = tbody.insertRow();
+        displayedFields.forEach(field => {
+            const cell = row.insertCell();
+            cell.textContent = item[field];
+        });
+    });
 }
